eb4e4c129ff85d4571fffa7a60ba2863aea88992
Дід <89632177+haloanglsx@users.noreply.github.com>
2024-08-29 19:00:13 +0300

    Update TechnologyManager.h

---
+++ TechnologyManager.h
@@ -0,0 +1,22 @@
+#ifndef TECHNOLOGYMANAGER_H
+#define TECHNOLOGYMANAGER_H
+
+#include "Technology.h"
+#include <vector>
+#include <string>
+
+class TechnologyManager {
+private:
+    std::vector<Technology*> technologies;
+
+public:
+    ~TechnologyManager();
+    void saveToFile(const std::string& filename) const;
+    void loadFromFile(const std::string& filename);
+    void addTechnology(const std::string& name, const std::string& type);
+    void displayAll() const;
+    void editTechnology(int index);
+    void deleteTechnology(int index);
+};
+
+#endif // TECHNOLOGYMANAGER_H
