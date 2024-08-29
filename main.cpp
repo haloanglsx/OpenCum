a7212ff679ecaa6cae21a2d6f1621959f7f0553d
Дід <89632177+haloanglsx@users.noreply.github.com>
2024-08-29 18:25:05 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,36 +1,36 @@
-#include <istream>
-#include <TechnologyManager.h>
+#include <iostream>
+#include "TechnologyManager.h"
 
 int main() {
     TechnologyManager manager;
 
     int choice;
     do {
-        std::count << "1. Add Technology \n"
-        std::count << "2. Display All Technologies\n"
-        std::count << "3. Edit Technology\n"
-        std::count << "4. Delete Technology\n"
-        std::count << "5. Exit\n\n"
-        std::count << "Enter youre choice :"
+        std::cout << "1. Add Technology\n";
+        std::cout << "2. Display All Technologies\n";
+        std::cout << "3. Edit Technology\n";
+        std::cout << "4. Delete Technology\n";
+        std::cout << "5. Exit\n";
+        std::cout << "Enter your choice: ";
         std::cin >> choice;
         std::cin.ignore();
 
         switch (choice) {
-            case 1:{
-                std::string name , type;
-                std::cout << "Enter technology name :";
+            case 1: {
+                std::string name, type;
+                std::cout << "Enter technology name: ";
                 std::getline(std::cin, name);
-                std::cout << "Enter technology type :";
-                std::getline(std: cin , type);
+                std::cout << "Enter technology type (Frontend/Backend): ";
+                std::getline(std::cin, type);
                 manager.addTechnology(name, type);
                 break;
             }
             case 2:
-                manager.dispayALL();
+                manager.displayAll();
                 break;
             case 3: {
                 int index;
-                sdt::cout << "Enter index technology to edit :";
+                std::cout << "Enter index of technology to edit: ";
                 std::cin >> index;
                 std::cin.ignore();
                 manager.editTechnology(index);
@@ -38,20 +38,21 @@ int main() {
             }
             case 4: {
                 int index;
-                std::cout << "Enter index technology delete :";
+                std::cout << "Enter index of technology to delete: ";
                 std::cin >> index;
                 std::cin.ignore();
                 manager.deleteTechnology(index);
                 break;
             }
             case 5:
-                std::cout << "Exiting...";
+                std::cout << "Exiting...\n";
                 break;
             default:
-                std::cout << "Invalid choice. please try again..\n";
+                std::cout << "Invalid choice. Please try again.\n";
                 break;
         }
-    }while (choice !=5);
-return 0
 
+    } while (choice != 5);
+
+    return 0;
 }
